162b810064fac196b52d291855cd09ecdebb6115
Natasha Onsongo <kemmienatasha@gmail.com>
2025-04-20 13:40:34 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from './Links';
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={props.github} target="_blank" rel="noopener noreferrer">
+        {props.github}
+      </a>
+      <br />
+      <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
+        {props.linkedin}
+      </a>
     </div>
   );
 }
